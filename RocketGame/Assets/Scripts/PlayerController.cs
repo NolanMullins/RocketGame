3f60e90ff7a52e5758c8883d4c329485cb039044
NolanMullins <nolan.mullins@me.com>
2016-04-18 23:47:10 -0400

    stars + basic astroids + updated movement

--- RocketGame/Assets/Scripts/PlayerController.cs
+++ RocketGame/Assets/Scripts/PlayerController.cs
@@ -1,51 +1,93 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float roationRate;
     public float velocity;
 
     private float rotationAngle;
+    private float rotationAngleVelocity;
 
     private Rigidbody2D myBody;
 
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         myBody = GetComponent<Rigidbody2D>();
-        rotationAngle = Mathf.PI*0.5f;
+        rotationAngle = Mathf.PI * 0.5f;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        //user Input
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool flag = true;
+        bool left = false;
+        bool right = false;
+        foreach (Touch touch in Input.touches)
         {
-            //myBody.transform.Rotate(Vector3.forward * Time.deltaTime * roationRate);
-            rotationAngle += roationRate * Time.deltaTime;
-            if (rotationAngle >= 180)
-                rotationAngle = 180;
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            //myBody.transform.Rotate(Vector3.back * Time.deltaTime * roationRate);
-            rotationAngle -= roationRate * Time.deltaTime;
-            if (rotationAngle <= 0)
-                rotationAngle = 0;
+            if (touch.position.x < Screen.width / 2)
+            {
+                left = true;
+            }
+            else if (touch.position.x > Screen.width / 2)
+            {
+                right = true;
+            }
         }
 
-        myBody.velocity = new Vector2(Mathf.Cos(rotationAngle)*velocity, Mathf.Sin(rotationAngle)* velocity);
+        //user Input
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || left)
+        {
+            //myBody.transform.Rotate(Vector3.forward * Time.deltaTime * roationRate);
+            rotationAngleVelocity += 0.05f*(roationRate * Time.deltaTime);
+            if (rotationAngleVelocity > roationRate * Time.deltaTime)
+            {
+                rotationAngleVelocity = roationRate * Time.deltaTime;
+            }
+            flag = false;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || right)
+        {
+            //myBody.transform.Rotate(Vector3.back * Time.deltaTime * roationRate);
+            rotationAngleVelocity += 0.05f*(-roationRate * Time.deltaTime);
+            if (rotationAngleVelocity < -roationRate * Time.deltaTime)
+            {
+                rotationAngleVelocity = -roationRate * Time.deltaTime;
+            }
+            flag = false;
+        }
+        if (flag && rotationAngleVelocity != 0)
+            rotationAngleVelocity -= 0.04f*Time.deltaTime*(rotationAngleVelocity/Mathf.Abs(rotationAngleVelocity));
+            
+        rotationAngle += rotationAngleVelocity;
+        if (rotationAngle >= 180)
+            rotationAngle = 180;
+        if (rotationAngle <= 0)
+            rotationAngle = 0;
+
+        float xVel = Mathf.Cos(rotationAngle) * velocity;
+        myBody.velocity = new Vector2(xVel, Mathf.Sin(rotationAngle) * velocity);
 
         Vector2 moveDirection = myBody.velocity;
         if (moveDirection != Vector2.zero)
         {
-            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg -90;
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg - 90;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
 
-        myBody.velocity = new Vector2(Mathf.Cos(rotationAngle) * velocity, 0);
+        myBody.velocity = new Vector2(xVel, 0);
 
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Astroids")
+        {
+            other.gameObject.SetActive(false);
+        }
     }
 }
