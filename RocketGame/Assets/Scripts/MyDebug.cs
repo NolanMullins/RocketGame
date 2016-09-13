4df5e2cfcb422b23df07c4fad7d2983f3395ad98
NolanMullins <nolan.mullins@me.com>
2016-09-13 00:01:35 -0400

    Added fps counter for debugging

---
+++ RocketGame/Assets/Scripts/MyDebug.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MyDebug : MonoBehaviour {
+
+    public Text logger;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        logger.text = Mathf.Round(1.0f / Time.smoothDeltaTime) + "";
+    }
+}
