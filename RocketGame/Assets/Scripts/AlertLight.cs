f5ebe0680fd61d380d317782b8f3e78a72bce8bf
NolanMullins <nolan.mullins@me.com>
2016-09-15 21:02:31 -0400

    Added warning lights + fixed laser picking up objects

---
+++ RocketGame/Assets/Scripts/AlertLight.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AlertLight : MonoBehaviour {
+
+    public Image light;
+
+    private float timer;
+    private float alpha;
+    private Color c;
+    private bool running;
+
+	// Use this for initialization
+	void Start () {
+        running = false;
+        timer = 0;
+        setColor(0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (running)
+            if (timer < 3.0f)
+            {
+                timer += Time.deltaTime;
+                alpha = -.4f * Mathf.Cos(4.19f * timer) + 0.4f; //6.28 -> 3 || 4.19 -> 2
+                setColor(alpha);
+            }
+            else
+            {
+                endAnimation();
+            }
+        
+
+	}
+
+    private void setColor(float alpha)
+    {
+        c = new Color(255, 255, 255, alpha);
+        light.color = c;
+    }
+
+    public void endAnimation()
+    {
+        running = false;
+        setColor(0);
+    }
+
+    public void startAnimation()
+    {
+        running = true;
+        timer = 0;
+        alpha = 0;
+    }
+}
--- RocketGame/Assets/Scripts/EnemyGenerator.cs
+++ RocketGame/Assets/Scripts/EnemyGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class EnemyGenerator : MonoBehaviour {
 
@@ -8,12 +9,16 @@ public class EnemyGenerator : MonoBehaviour {
     public List<GameObject> enemyPool;
     public float spawnTimer;
 
+    //notification lights
+    public AlertLight leftLight;
+    public AlertLight rightLight;
+
     private float timer;
     private float gameTime;
 
 	// Use this for initialization
 	void Start () {
-
+        
     }
 	
 	// Update is called once per frame
@@ -29,12 +34,23 @@ public class EnemyGenerator : MonoBehaviour {
 
     private void spawnEnemy()
     {
-        Transform  pos = spawns[Random.Range(0, spawns.Count)].transform;
-        for(int a = 0; a < enemyPool.Count; a++)
+        Transform pos = spawns[Random.Range(0, spawns.Count)].transform;
+        if (pos.position.x < 0)
+            leftLight.startAnimation();
+        else
+            rightLight.startAnimation();
+
+            StartCoroutine(spawnSeq(pos));        
+    }
+
+    IEnumerator spawnSeq(Transform pos)
+    {
+        for (int a = 0; a < enemyPool.Count; a++)
         {
             if (!enemyPool[a].activeInHierarchy)
             {
                 //Debug.Log("Spawning: "+a);
+                yield return new WaitForSeconds(3);
                 GameObject enemy = enemyPool[a];
                 enemy.SetActive(true);
                 enemy.GetComponent<EnemyController>().spawn(new Vector3(pos.position.x, pos.position.y, 0));
@@ -43,8 +59,8 @@ public class EnemyGenerator : MonoBehaviour {
             {
                 //Debug.Log("Cant Spawn: " + a);
             }
-                
         }
+
         
     }
 
